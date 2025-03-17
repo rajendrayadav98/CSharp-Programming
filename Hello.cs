fc757aa49a4c20e615e395def2f235b713d837d6
rajendrayadav98 <rajendra44556@gmail.com>
2025-03-17 23:26:52 +0530

    Print Hello

---
+++ Hello.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RajendraConsoleApp.CSharp11
+{
+    public class HelloWorld
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello world");
+        }
+
+    }
+}
