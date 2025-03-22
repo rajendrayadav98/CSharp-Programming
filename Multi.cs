1e0946ec0daceafe54237b5d38b0d13bc3db1cda
rajendrayadav98 <rajendra44556@gmail.com>
2025-03-22 19:37:18 +0530

    Multiple of two number

---
+++ Multi.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace RajendraConsoleApp.FunctionAndArray
+{
+    internal class SumOfTwoNumber
+    { 
+        static void Mul()
+        {
+            int a = 50;
+            int b = 20;
+            int c = a*b;
+            Console.Write("Multiple of two number:"+c);
+        }
+        static void Main(string[] args)
+        {
+            Mul();
+        }
+    }
+}
