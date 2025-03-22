977518087dcff22e85f566220e5325a058585fbf
rajendrayadav98 <rajendra44556@gmail.com>
2025-03-22 19:37:48 +0530

    Sum of two number

---
+++ SumOfTwoNumber.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace RajendraConsoleApp.FunctionAndArray
+{
+    internal class SumOfTwoNumber
+    { 
+        static void Add()
+        {
+            int a = 10;
+            int b = 20;
+            int c = a+b;
+            Console.Write("Sum of two number:"+c);
+        }
+        static void Main(string[] args)
+        {
+            Add();
+        }
+    }
+}
