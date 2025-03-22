345214c30339f543e2feaa4ce828e7f9010318b2
rajendrayadav98 <rajendra44556@gmail.com>
2025-03-22 19:38:11 +0530

    Substraction of two number

---
+++ Subtraction.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace RajendraConsoleApp.FunctionAndArray
+{
+    internal class SumOfTwoNumber
+    { 
+        static void Sub()
+        {
+            int a = 50;
+            int b = 20;
+            int c = a-b;
+            Console.Write("Sub of two number:"+c);
+        }
+        static void Main(string[] args)
+        {
+            Sub();
+        }
+    }
+}
