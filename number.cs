0aab5acd383eb30fddd6ceff89f4348564c8ae88
rajendrayadav98 <rajendra44556@gmail.com>
2025-03-24 22:41:13 +0530

    Number print

---
+++ number.cs
@@ -0,0 +1,14 @@
+using System;
+
+class ForLoopExample
+{
+    static void Main()
+    {
+        Console.WriteLine("Using a for loop to print the first 10 numbers:");
+        
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
