5da6f0687e2a2fe86ebf1c5c37d56b7e89005323
rajendrayadav98 <rajendra44556@gmail.com>
2025-03-29 20:36:48 +0530

    second class under project

---
+++ second.cs
@@ -0,0 +1,12 @@
+using System;
+namespace FirstProjet
+{
+   internal class Class1
+   {
+      static void Main()
+      {
+         Console.WriteLine("Second class under the project.");
+         Console.ReadLine();
+      }
+   }
+}
