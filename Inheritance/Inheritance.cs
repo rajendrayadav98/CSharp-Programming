feee01252c0ed32e190794aef59f52a8478e8eb0
rajendrayadav98 <rajendra44556@gmail.com>
2025-04-05 19:52:41 +0530

    Inheritance

---
+++ Inheritance/Inheritance.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test1
+{
+    internal class Vehicle
+    {
+        public int speed;
+        public string fuel;
+
+        public Vehicle(int speed, string fuel)
+        {
+            this.speed = speed;
+            this.fuel = fuel;
+        }
+    }
+    class Car : Vehicle
+    {
+        public string carType;
+        public Car(int speed, string fuel, string carType) : base(speed, fuel)
+        {
+            this.carType = carType;
+        }
+        public void ShowCarDetails()
+        {
+            Console.WriteLine("Car Type: " + carType);
+            Console.WriteLine("fuel: " + fuel);
+            Console.WriteLine("Speed: " + speed);
+        }
+        }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car c = new Car(120, "Sedan", "Petrol");
+            c.ShowCarDetails();
+            Console.ReadLine();
+        }
+    }
+
+}
