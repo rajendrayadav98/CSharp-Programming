adec9c2f1b554c688de420aecf738f0de4e4a3b9
rajendrayadav98 <rajendra44556@gmail.com>
2025-04-22 21:28:28 +0530

    example of List

---
+++ Collection/example8.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> numbers = new List<int>();
+        numbers.Add(5);
+        numbers.Add(10);
+        numbers.Add(15);
+
+        foreach (int num in numbers)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
